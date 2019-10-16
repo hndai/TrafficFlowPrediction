c70b55af658b846880ca592c37c01e9c1f764a49
zhoujunhao <776253638@qq.com>
2019-10-16 23:13:19 +0800

    Add files via upload

---
+++ data/data_bike.py
@@ -0,0 +1,104 @@
+"""
+Processing the data
+"""
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
+
+def process_data(train, test, lags):
+    """Process data
+    Reshape and split train\test data.
+
+    # Arguments
+        train: String, name of .csv train file.
+        test: String, name of .csv test file.
+        lags: integer, time lag.
+    # Returns
+        X_train: ndarray.
+        y_train: ndarray.
+        X_test: ndarray.
+        y_test: ndarray.
+        scaler: StandardScaler.
+    """
+    #attr = 'Lane 1 Flow (Veh/5 Minutes)'
+    #attr = 'East'
+    attr = 'Fremont Bridge East Sidewalk'
+    df1 = pd.read_csv(train, encoding='utf-8').fillna(0)
+    df2 = pd.read_csv(test, encoding='utf-8').fillna(0)
+
+    # scaler = StandardScaler().fit(df1[attr].values)
+    scaler = MinMaxScaler(feature_range=(0, 1)).fit(df1[attr].values.reshape(-1, 1))
+    flow1 = scaler.transform(df1[attr].values.reshape(-1, 1)).reshape(1, -1)[0]
+    flow2 = scaler.transform(df2[attr].values.reshape(-1, 1)).reshape(1, -1)[0]
+    #print("flow1:",flow1)
+    #print("flow2:",flow2)
+    
+    train, test = [], []
+    for i in range(lags, len(flow1)):
+        train.append(flow1[i - lags: i + 1])
+    for i in range(lags, len(flow2)):
+        test.append(flow2[i - lags: i + 1])
+
+    train = np.array(train)
+    test = np.array(test)
+    np.random.shuffle(train)
+
+    X_train = train[:, :-1]
+    #y_train = train[:, :-1]
+    y_train = train[:, -1]
+    X_test = test[:, :-1]
+    y_test = test[:, -1]
+    #y_test = test[:, :-1]
+    #print("X_train_data:",X_train)
+    print("X+train_data_shape:",X_train.shape)
+    print("y+train_data_shape:",y_train.shape)
+    return X_train, y_train, X_test, y_test, scaler
+
+def process_data_wd(train, test, lags):
+    """Process data
+    Reshape and split train\test data.
+
+    # Arguments
+        train: String, name of .csv train file.
+        test: String, name of .csv test file.
+        lags: integer, time lag.
+    # Returns
+        X_train: ndarray.
+        y_train: ndarray.
+        X_test: ndarray.
+        y_test: ndarray.
+        scaler: StandardScaler.
+    """
+    attr = 'Fremont Bridge East Sidewalk'
+    df1 = pd.read_csv(train, encoding='utf-8').fillna(0)
+    df2 = pd.read_csv(test, encoding='utf-8').fillna(0)
+
+    # scaler = StandardScaler().fit(df1[attr].values)
+    scaler = MinMaxScaler(feature_range=(0, 1)).fit(df1[attr].values.reshape(-1, 1))
+    flow1 = scaler.transform(df1[attr].values.reshape(-1, 1)).reshape(1, -1)[0]
+    flow2 = scaler.transform(df2[attr].values.reshape(-1, 1)).reshape(1, -1)[0]
+    #print("flow1:",flow1)
+    #print("flow2:",flow2)
+    
+    train, test = [], []
+    for i in range(lags, len(flow1)):
+        train.append(flow1[i - lags: i + 1])
+    for i in range(lags, len(flow2)):
+        test.append(flow2[i - lags: i + 1])
+
+    train = np.array(train)
+    test = np.array(test)
+    np.random.shuffle(train)
+
+    X_train = train[:, :-1]
+    y_train = train[:, :-1]
+    #y_train = train[:, -1]
+    X_test = test[:, :-1]
+    y_test = test[:, -1]
+    #y_test = test[:, :-1]
+    #print("X_train_data:",X_train)
+    print("X+train_data_shape:",X_train.shape)
+    print("y+train_data_shape:",y_train.shape)
+    print("y+test_data_shape:",y_test.shape)
+    return X_train, y_train, X_test, y_test, scaler
